7dacc399bed4d9e71f8fbf6d05b0af958599276e
David Lemayian <david@on.co.ke>
2016-09-05 13:56:07 +0300

    Header now fills page.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+
+// Make header fit height
+if ($('home .header').outerHeight(true) < $(window).height()) {
+  var header_height = $('.header').height();
+  var window_height = $(window).height();
+
+  var header_padding_top = window_height/2 - header_height/2;
+  var header_padding_bottom = $('home .header').outerHeight();
+  
+  $('.header').css('padding-top', header_padding_top);
+  $('.header').outerHeight(window_height);
+  
+}
+
+// Activate tootltip
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+
